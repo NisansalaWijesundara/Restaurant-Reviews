82c17a5f1a1635adb5c5890a5d4c23378f2b8eb6
Nisansala <nisansalawijesundara@gmail.com>
2019-03-22 22:32:45 +0530

    Activate & fetch sw.

--- sw.js
+++ sw.js
@@ -21,16 +21,46 @@ let urlsToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-    
-    e.waitUntil(
-    		caches.open(cacheName).then(function(cache) {
-    			return cache.addAll(urlsToCache);
-            })
-        );
-      });
-/*self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activated');
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(cacheNames.map(function(thisCacheName) {
+        if (thisCacheName != cacheName) {
+          console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+          return caches.delete(thisCacheName);
+        }
+      }));
+    })
+  );
+});
+
 self.addEventListener('fetch', function(e) {
-	console.log('[ServiceWorker] Fetch', e.request.url);
-});*/
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response) {
+        console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+        return response;
+      } else {
+        console.log("[ServiceWorker]could not Found in Cache", e.request.url, response);
+        return fetch(e.request).then(function(response) {
+            let responseClone = response.clone();
+            caches.open('cacheName').then(function(cache) {
+              cache.put(e.request, responseClone);
+            })
+            return response;
+          })
+          .catch(function(err) {
+            console.error(err);
+          });
+      }
+    })
+  );
+});
